8b6c9750d8c23cc4cfdac0b9cf2ca11be75a3c39
montespatricio <patricio.montes@apexamerica.com>
2021-09-28 15:48:55 -0300

    Added: api .Net Core 3.1 template with ocelot 16.0.1 example.

---
+++ apex-apigateway-ocelot/Program.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
+
+namespace apex_apigateway_ocelot
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureAppConfiguration((hosting, config) =>
+                {
+                    config.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+                })
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
