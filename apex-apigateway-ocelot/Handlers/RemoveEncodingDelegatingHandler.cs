b3465a7f35eab0a9e48a81b3525759649d29f6bf
Patricio Montes <patricio.montes@apexamerica.com>
2022-08-04 10:35:17 -0300

    RA-445. Added: Get tokens configurations. Get Secret Key configurations.

--- apex-apigateway-ocelot/Handlers/RemoveEncodingDelegatingHandler.cs
+++ apex-apigateway-ocelot/Handlers/RemoveEncodingDelegatingHandler.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using apex_apigateway_ocelot.Security;
+using Google.Apis.Auth.OAuth2;
+using System;
 using System.Net.Http;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,6 +12,18 @@ namespace apex_apigateway_ocelot.Handlers
         protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
         {
             request.Headers.AcceptEncoding.Clear();
+            IAPClient iap = new IAPClient();
+            SecretManagerService secretManagerService = new SecretManagerService();
+            //string secret_token = secretManagerService.GetSecretKey();
+            // ID Cliente Lab Pocs for Service Account ***REMOVED_SERVICE_ACCOUNT***
+            //var response = iap.GetOidcTokenAsync("***REMOVED_OIDC_CLIENT_ID***", default);
+            // ID Cliente Lab Martin Leal
+            OidcToken oidcToken = await iap.GetOidcTokenAsync("***REMOVED_OIDC_CLIENT_ID***", cancellationToken);
+
+            //***REMOVED_OIDC_CLIENT_ID***
+            //OidcToken oidcToken = await iap.GetOidcTokenAsync("***REMOVED_OIDC_CLIENT_ID***", cancellationToken);
+            string token = await oidcToken.GetAccessTokenAsync(cancellationToken);
+            //Console.Write("Bearer Token:" + token);
             return await base.SendAsync(request, cancellationToken);
         }
     }
